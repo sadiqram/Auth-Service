b998e2dd0afda2cc93073420ae2a5371a32a4455
sadiqram <abdulrahmansadiq13@gmail.com>
2026-01-12 15:01:20 -0600

    update depencies and test server

--- app/api/src/server.ts
+++ app/api/src/server.ts
@@ -2,11 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 app.get("/health", (req, res) => {
-    res.status(200).json({status: "ok"});
+    // res.status(200).json({status: "ok"});
     res.json({ message: "Hello World" });
   
 });
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
