ea4fbc7bd3f072bae8eb752b76222adcd047da14
sadiqram <abdulrahmansadiq13@gmail.com>
2026-01-12 14:31:23 -0600

    fix typo in dockerfiles, implement server and test

--- app/api/src/server.ts
+++ app/api/src/server.ts
@@ -8,4 +8,8 @@ app.get("/health", (req, res) => {
   
 });
 
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(` Server is running on port ${PORT}`);
+});
 export default app;
