2502451653098de13421da15d9115d2a4b6101d7
sadiqram <abdulrahmansadiq13@gmail.com>
2026-01-13 18:00:46 -0600

    Add database connection and perform health check

--- app/api/src/server.ts
+++ app/api/src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import pool from "./db/db";
 
 const app = express();
 
@@ -6,10 +7,16 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/health", (req, res) => {
-    // res.status(200).json({status: "ok"});
-    res.json({ message: "Hello World" });
-  
+app.get("/health", async (req, res) => {
+   let dbHealthy = true;
+   try{
+    // DB ping
+    await pool.query("SELECT 1");
+   } catch (error) {
+    dbHealthy = false;
+    console.error("DB ping failed", error);
+   }
+   res.status(dbHealthy ? 200 : 503).json({ status: dbHealthy ? "ok" : "error", db: dbHealthy ? "connected" : "disconnected" });
 });
 
 
