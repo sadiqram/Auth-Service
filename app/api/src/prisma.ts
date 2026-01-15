27b021f56f6474fdc76426713cd713f83c4a26ba
sadiqram <abdulrahmansadiq13@gmail.com>
2026-01-14 19:11:24 -0600

    resolved prisma7 prismaclient instantiation error link has solution https://www.prisma.io/docs/orm/more/upgrade-guides/upgrading-versions/upgrading-to-prisma-7#driver-adapters-and-client-instantiation

--- app/api/src/prisma.ts
+++ app/api/src/prisma.ts
@@ -9,6 +9,8 @@ const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!, // same DATABASE_URL you already use
 });
 
+
+
 //  Prisma 7: you MUST pass options (adapter or accelerateUrl)
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
